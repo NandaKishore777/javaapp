f4764caa89e33fbd0293f5922ac06f4719e2322c
NandaKishore777 <pnk798977@gmail.com>
2023-02-28 22:36:57 +0530

    java

---
+++ apply-templates.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+[ -f versions.json ] # run "versions.sh" first
+
+jqt='.jq-template.awk'
+if [ -n "${BASHBREW_SCRIPTS:-}" ]; then
+	jqt="$BASHBREW_SCRIPTS/jq-template.awk"
+elif [ "$BASH_SOURCE" -nt "$jqt" ]; then
+	# https://github.com/docker-library/bashbrew/blob/master/scripts/jq-template.awk
+	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/9f6a35772ac863a0241f147c820354e4008edf38/scripts/jq-template.awk'
+fi
+
+if [ "$#" -eq 0 ]; then
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
+fi
+
+generated_warning() {
+	cat <<-EOH
+		#
+		# NOTE: THIS DOCKERFILE IS GENERATED VIA "apply-templates.sh"
+		#
+		# PLEASE DO NOT EDIT IT DIRECTLY.
+		#
+
+	EOH
+}
+
+for version; do
+	export version
+
+	rm -rf "$version/"
+
+	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
+	eval "variants=( $variants )"
+
+	for javaType in jdk jre; do
+		export javaType
+
+		if ! hasJavaType="$(jq -r '.[env.version] | if has(env.javaType) then "1" else "" end' versions.json)" || [ -z "$hasJavaType" ]; then
+			continue
+		fi
+
+		for variant in "${variants[@]}"; do
+			export variant
+
+			if [ "$javaType" = 'jre' ] && [[ "$variant" == oraclelinux* ]]; then
+				continue # no Oracle-based JRE images (for now? gotta figure a few things out to do that)
+			fi
+
+			dir="$version/$javaType/$variant"
+			mkdir -p "$dir"
+
+			case "$variant" in
+				windows/*)
+					variant="$(basename "$dir")" # "buster", "windowsservercore-1809", etc
+					windowsVariant="${variant%%-*}" # "windowsservercore", "nanoserver"
+					windowsRelease="${variant#$windowsVariant-}" # "ltsc2022", "1809", etc
+					windowsVariant="${windowsVariant#windows}" # "servercore", "nanoserver"
+					export windowsVariant windowsRelease
+					template='Dockerfile-windows.template'
+					;;
+
+				*)
+					template='Dockerfile-linux.template'
+					;;
+			esac
+
+			echo "processing $dir ..."
+
+			{
+				generated_warning
+				gawk -f "$jqt" "$template"
+			} > "$dir/Dockerfile"
+		done
+	done
+done
---
+++ versions.sh
@@ -0,0 +1,179 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+	json='{}'
+else
+	json="$(< versions.json)"
+fi
+versions=( "${versions[@]%/}" )
+
+tmp="$(mktemp -d)"
+rmTmp="$(printf 'rm -rf %q' "$tmp")"
+trap "$rmTmp" EXIT
+
+_get() {
+	local url="$1"; shift
+	local file="${url////_}"
+	file="${file//%/_}"
+	file="${file//+/_}"
+	file="${file//:/_}"
+	file="$tmp/$file"
+	if [ ! -s "$file" ]; then
+		curl -fsSL "$url" -o "$file" --retry 5 || return 1
+	fi
+	if [ "$#" -gt 0 ]; then
+		grep "$@" "$file"
+	else
+		cat "$file"
+	fi
+}
+
+abs-url() {
+	local url="$1"; shift
+	local base="$1"; shift
+
+	case "$url" in
+		http://* | https://* ) ;;
+
+		/*)
+			local extra="${base#*://*/}"
+			local baseBase="${base%$extra}"
+			baseBase="${baseBase%/}"
+			url="$baseBase$url"
+			;;
+
+		*)
+			echo >&2 "error: TODO parse '$url' relative to '$base'"
+			exit 1
+			;;
+	esac
+
+	echo "$url"
+}
+
+jdk-java-net-download-url() {
+	local javaVersion="$1"; shift
+	local fileSuffix="$1"; shift
+	_get "https://jdk.java.net/$javaVersion/" \
+		-Eom1 "https://download.java.net/[^\"]+$fileSuffix"
+}
+
+jdk-java-net-download-version() {
+	local javaVersion="$1"; shift
+	local downloadUrl="$1"; shift
+
+	downloadVersion="$(grep -Eom1 "openjdk-$javaVersion[^_]*_" <<<"$downloadUrl")" || return 1
+	downloadVersion="${downloadVersion%_}"
+	downloadVersion="${downloadVersion#openjdk-}"
+
+	echo "$downloadVersion"
+}
+
+# see https://stackoverflow.com/a/2705678/433558
+sed_escape_rhs() {
+	sed -e 's/[\/&]/\\&/g' <<<"$*" | sed -e ':a;N;$!ba;s/\n/\\n/g'
+}
+sed_s() {
+	local lhs="$1"; shift
+	local rhs="$1"; shift
+	rhs="$(sed_escape_rhs "$rhs")"
+	echo -n "s/$lhs/$rhs/g"
+}
+sed_s_pre() {
+	local lhs="$1"; shift
+	local rhs="$1"; shift
+	rhs="$(sed_escape_rhs "$rhs")"
+	echo -n "s/^($lhs) .*$/\1 $rhs/"
+}
+
+for version in "${versions[@]}"; do
+	export version
+	doc='{}'
+	possibleArches=(
+		# https://jdk.java.net/20/
+		# https://jdk.java.net/21/
+		'linux-aarch64'
+		'linux-x64'
+		'linux-x64-musl'
+		'windows-x64'
+	)
+	for arch in "${possibleArches[@]}"; do
+		downloadSuffix="_${arch}_bin"
+		case "$arch" in
+			linux-*) downloadSuffix+='.tar.gz'; bashbrewArch= ;;
+			windows-*) downloadSuffix+='.zip'; bashbrewArch='windows-' ;;
+			*) echo >&2 "error: unknown Oracle arch: '$arch'"; exit 1 ;;
+		esac
+		jqExprPrefix=
+		if [[ "$arch" == *-musl ]]; then
+			jqExprPrefix='.alpine'
+		fi
+		if downloadUrl="$(jdk-java-net-download-url "$version" "$downloadSuffix")" \
+			&& [ -n "$downloadUrl" ] \
+			&& downloadSha256="$(_get "$downloadUrl.sha256")" \
+			&& [ -n "$downloadSha256" ] \
+		; then
+			downloadVersion="$(jdk-java-net-download-version "$version" "$downloadUrl")"
+			currentVersion="$(jq <<<"$doc" -r "$jqExprPrefix.version // \"\"")"
+			if [ -n "$currentVersion" ] && [ "$currentVersion" != "$downloadVersion" ]; then
+				echo >&2 "error: Oracle version mismatch: '$currentVersion' vs '$downloadVersion'"
+				exit 1
+			elif [ -z "$currentVersion" ]; then
+				echo "$version: $downloadVersion${jqExprPrefix:+ (alpine)}"
+			fi
+			case "$arch" in
+				*-aarch64*) bashbrewArch+='arm64v8' ;;
+				*-x64*) bashbrewArch+='amd64' ;;
+				*) echo >&2 "error: unknown Oracle arch: '$arch'"; exit 1 ;;
+			esac
+			export arch bashbrewArch downloadUrl downloadSha256 downloadVersion
+			doc="$(jq <<<"$doc" -c '
+				'"$jqExprPrefix"'.version = env.downloadVersion
+				| '"$jqExprPrefix"'.jdk.arches[env.bashbrewArch] = {
+					url: env.downloadUrl,
+					sha256: env.downloadSha256,
+				}
+			')"
+		fi
+	done
+
+	if ! jq <<<"$doc" -e '[ .. | objects | select(has("arches")) | .arches | has("amd64") ] | all' &> /dev/null; then
+		echo >&2 "error: missing 'amd64' for '$version'; cowardly refusing to continue! (because this is almost always a scraping flake or similar bug)"
+		exit 1
+	fi
+
+	json="$(jq <<<"$json" -c --argjson doc "$doc" '
+		.[env.version] = $doc + {
+			variants: [
+				(
+					"8",
+					"7"
+				| "oraclelinux" + .),
+				(
+					"bullseye",
+					"buster"
+				| ., "slim-" + .),
+				if $doc.alpine then
+					"3.17",
+					"3.16"
+				| "alpine" + . else empty end,
+				if $doc.jdk.arches | keys | any(startswith("windows-")) then
+					(
+						"ltsc2022",
+						"1809"
+					| "windows/windowsservercore-" + .),
+					(
+						"1809"
+					| "windows/nanoserver-" + .)
+				else empty end
+			],
+		}
+	')"
+done
+
+jq <<<"$json" -S . > versions.json
